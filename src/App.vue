ef702256066c352738edbf8e158920027f4cc517
“Jimmy <fabricioortega2011@gmail.com>
2024-06-07 19:47:53 -0500

    Taller18 parte 2

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,30 @@
 <template>
-
-  
-  <img src="./assets/logo.png" alt="NONE">
-  <Contador titulo="Estudiante" :num="-1"/>   
-  <Contador titulo="Profesor" />
-  <Contador v-bind:num="calcularValor()" />
-
-  <!-- La directiva v-bind:num se llama métodos o cualquier cosa que sea java Script -->
-  
-  
-  
-  </template>
+  <Pregunta />
+</template>
   
   <script>
-  
-  import Contador from "./components/Contador.vue" 
-  
-  
-  export default {
-    name: 'App',
-    components: {
-      Contador,  
-  
+import Pregunta from "./components/Pregunta.vue";
+
+export default {
+  name: "App",
+  components: {
+    Pregunta,
+  },
+  methods: {
+    calcularValor() {
+      return 10 + 2;
     },
-    methods:{
-      calcularValor(){
-        return 10+2;
-      }
-    }
-  }
-  </script>
+  },
+};
+</script>
   
   <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  </style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Pregunta.vue
@@ -0,0 +1,24 @@
+<template>
+  <img
+    src="https://yesno.wtf/assets/no/22-8806dbccb1edf544723b7f095ff722e8.gif"
+    alt="No se pudo"
+  />
+
+  <div class="oscuro"></div>
+
+  <div class="pregunta-container">
+    <input type="text" placeholder="Hazme una pregunta" />
+    <p>Recuerda terminar la pregunta con el signo de interrogración(?)</p>
+  </div>
+  <div>
+    <h2>Voy a pasar de semestre?</h2>
+    <h1>SI, NO,......</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
